bbdbfad9365a2f366e4ff39cd168cba9b867ca6d
Stratos <anthopoue@csd.auth.gr>
2022-03-08 23:09:52 +0200

    winner at 100

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // 2. Check if player's score is >=100
-    if (scores[activePlayer] >= 30) {
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       document.querySelector('.dice').classList.add('hidden');
       playing = false;
